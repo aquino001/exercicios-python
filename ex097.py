d0783b6cba2c0c290222e21bfc0244f2ec9bad3e
Aquino001 <luan.a.souza10@aluno.senai.br>
2024-11-09 09:54:46 -0300

    Um print especial

---
+++ ex097.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('-' * tam)
+    print(f'  {msg}')
+    print('-' * tam)
+
+
+#   Programa principal
+escreva('Olá Mundo')
+escreva('Vou me tornar programador')
+escreva('Oi')
